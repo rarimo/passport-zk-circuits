83c7d4990987a56491db043cc101b1a8b20ec3de
erdoganishe <khotyanmisha@gmail.com>
2024-09-16 16:42:45 +0300

    Refactor tests

--- tests/tests/registerIdentityTest.js
+++ tests/tests/registerIdentityTest.js
@@ -1,139 +1,139 @@
-// const chai = require("chai");
-// const path = require("path");
-// const wasm_tester = require("../index").wasm;
-// const c_tester = require("../index").c;
+const chai = require("chai");
+const path = require("path");
+const wasm_tester = require("../index").wasm;
+const c_tester = require("../index").c;
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const F1Field = require("ffjavascript").F1Field;
-// const Scalar = require("ffjavascript").Scalar;
-// exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
-// const Fr = new F1Field(exports.p);
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
 
-// const assert = chai.assert;
+const assert = chai.assert;
 
-// describe("Register identity test", function () {
-//     this.timeout(100000);
+describe("Register identity test", function () {
+    this.timeout(100000);
 
-// 	it("Ukraine test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/register_identity/input_ukr.json');
+	it("Ukraine test", async function () {
+		const testJson = path.join(__dirname, './inputs/register_identity/input_ukr.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/identityManagement/main_ukr.circom")
-// 			);
-//             const w = await circuit.calculateWitness({ 
-//                 dg1: input.dg1,
-//                 dg15: input.dg15,
-//                 encapsulatedContent: input.encapsulatedContent,
-//                 signedAttributes: input.signedAttributes,
-//                 signature: input.signature,
-//                 pubkey: input.pubkey,
-//                 skIdentity: input.skIdentity,
-//                 slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
-//                 slaveMerkleRoot: input.slaveMerkleRoot
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/identityManagement/main_ukr.circom")
+			);
+            const w = await circuit.calculateWitness({ 
+                dg1: input.dg1,
+                dg15: input.dg15,
+                encapsulatedContent: input.encapsulatedContent,
+                signedAttributes: input.signedAttributes,
+                signature: input.signature,
+                pubkey: input.pubkey,
+                skIdentity: input.skIdentity,
+                slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
+                slaveMerkleRoot: input.slaveMerkleRoot
 
-//             });
-// 			await circuit.checkConstraints(w);
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
 
-//     it("Denmark test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/register_identity/input_denmark.json');
+    it("Denmark test", async function () {
+		const testJson = path.join(__dirname, './inputs/register_identity/input_denmark.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/identityManagement/main_denmark.circom")
-// 			);
-//             const w = await circuit.calculateWitness({ 
-//                 dg1: input.dg1,
-//                 dg15: input.dg15,
-//                 encapsulatedContent: input.encapsulatedContent,
-//                 signedAttributes: input.signedAttributes,
-//                 signature: input.signature,
-//                 pubkey: input.pubkey,
-//                 skIdentity: input.skIdentity,
-//                 slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
-//                 slaveMerkleRoot: input.slaveMerkleRoot
-//             });
-// 			await circuit.checkConstraints(w);
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/identityManagement/main_denmark.circom")
+			);
+            const w = await circuit.calculateWitness({ 
+                dg1: input.dg1,
+                dg15: input.dg15,
+                encapsulatedContent: input.encapsulatedContent,
+                signedAttributes: input.signedAttributes,
+                signature: input.signature,
+                pubkey: input.pubkey,
+                skIdentity: input.skIdentity,
+                slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
+                slaveMerkleRoot: input.slaveMerkleRoot
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
 
-//     it("Ukraine corrupted test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/register_identity/input_ukr_corrupted.json');
+    it("Ukraine corrupted test", async function () {
+		const testJson = path.join(__dirname, './inputs/register_identity/input_ukr_corrupted.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/identityManagement/main_ukr.circom")
-// 			);
-//             const w = await circuit.calculateWitness({ 
-//                 dg1: input.dg1,
-//                 dg15: input.dg15,
-//                 encapsulatedContent: input.encapsulatedContent,
-//                 signedAttributes: input.signedAttributes,
-//                 signature: input.signature,
-//                 pubkey: input.pubkey,
-//                 skIdentity: input.skIdentity,
-//                 slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
-//                 slaveMerkleRoot: input.slaveMerkleRoot
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/identityManagement/main_ukr.circom")
+			);
+            const w = await circuit.calculateWitness({ 
+                dg1: input.dg1,
+                dg15: input.dg15,
+                encapsulatedContent: input.encapsulatedContent,
+                signedAttributes: input.signedAttributes,
+                signature: input.signature,
+                pubkey: input.pubkey,
+                skIdentity: input.skIdentity,
+                slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
+                slaveMerkleRoot: input.slaveMerkleRoot
 
-//             });
-// 			await circuit.checkConstraints(w);
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
 
-//     it("Denmark corrupted test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/register_identity/input_denmark_corrupted.json');
+    it("Denmark corrupted test", async function () {
+		const testJson = path.join(__dirname, './inputs/register_identity/input_denmark_corrupted.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/identityManagement/main_denmark.circom")
-// 			);
-//             const w = await circuit.calculateWitness({ 
-//                 dg1: input.dg1,
-//                 dg15: input.dg15,
-//                 encapsulatedContent: input.encapsulatedContent,
-//                 signedAttributes: input.signedAttributes,
-//                 signature: input.signature,
-//                 pubkey: input.pubkey,
-//                 skIdentity: input.skIdentity,
-//                 slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
-//                 slaveMerkleRoot: input.slaveMerkleRoot
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/identityManagement/main_denmark.circom")
+			);
+            const w = await circuit.calculateWitness({ 
+                dg1: input.dg1,
+                dg15: input.dg15,
+                encapsulatedContent: input.encapsulatedContent,
+                signedAttributes: input.signedAttributes,
+                signature: input.signature,
+                pubkey: input.pubkey,
+                skIdentity: input.skIdentity,
+                slaveMerkleInclusionBranches: input.slaveMerkleInclusionBranches,
+                slaveMerkleRoot: input.slaveMerkleRoot
 
-//             });
-// 			await circuit.checkConstraints(w);
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
 
 	
 	
-// });
+});
--- tests/tests/rsaTest.js
+++ tests/tests/rsaTest.js
@@ -1,66 +1,66 @@
-// const chai = require("chai");
-// const path = require("path");
-// const wasm_tester = require("../index").wasm;
-// const c_tester = require("../index").c;
-// const crypto = require('crypto');
+const chai = require("chai");
+const path = require("path");
+const wasm_tester = require("../index").wasm;
+const c_tester = require("../index").c;
+const crypto = require('crypto');
 
 
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const F1Field = require("ffjavascript").F1Field;
-// const Scalar = require("ffjavascript").Scalar;
-// exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
-// const Fr = new F1Field(exports.p);
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
 
-// const assert = chai.assert;
+const assert = chai.assert;
 
-// describe("RSA test", function () {
-//     this.timeout(100000);
+describe("RSA test", function () {
+    this.timeout(100000);
 
-// 	it("Rsa 2048 test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/rsa/rsa2048.json');
+	it("Rsa 2048 test", async function () {
+		const testJson = path.join(__dirname, './inputs/rsa/rsa2048.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/rsa/rsaVerify.circom")
-// 			);
-// 			const w = await circuit.calculateWitness({ 
-//                 pubkey: input.pubkey,
-//                 signature: input.signature,
-//                 hashed: input.hashed
-//             });
-// 			await circuit.checkConstraints(w);
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/rsa/rsaVerify.circom")
+			);
+			const w = await circuit.calculateWitness({ 
+                pubkey: input.pubkey,
+                signature: input.signature,
+                hashed: input.hashed
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
 
-//     it("Rsa 2048 corrupted test", async function () {
-// 		const testJson = path.join(__dirname, './inputs/rsa/rsa2048_corrupted.json');
+    it("Rsa 2048 corrupted test", async function () {
+		const testJson = path.join(__dirname, './inputs/rsa/rsa2048_corrupted.json');
 	
-// 		try {
-// 			const data = await fs.promises.readFile(testJson, 'utf8');
-// 			const input = JSON.parse(data);
+		try {
+			const data = await fs.promises.readFile(testJson, 'utf8');
+			const input = JSON.parse(data);
 	
-// 			const circuit = await wasm_tester(
-// 				path.join(__dirname, "../../circuits/rsa/rsaVerify.circom")
-// 			);
-// 			const w = await circuit.calculateWitness({ 
-//                 pubkey: input.pubkey,
-//                 signature: input.signature,
-//                 hashed: input.hashed
-//             });
-// 			await circuit.checkConstraints(w);
+			const circuit = await wasm_tester(
+				path.join(__dirname, "../../circuits/rsa/rsaVerify.circom")
+			);
+			const w = await circuit.calculateWitness({ 
+                pubkey: input.pubkey,
+                signature: input.signature,
+                hashed: input.hashed
+            });
+			await circuit.checkConstraints(w);
 	
-// 		} catch (err) {
-// 			console.error('Error:', err);
-// 			throw err;  
-// 		}
-// 	});
-// });
+		} catch (err) {
+			console.error('Error:', err);
+			throw err;  
+		}
+	});
+});
